713db968129485c8dcdeaeb32ec0f66c2488ccc9
Hugo BRETECHE <hubretec@e1r1p4.clusters.42paris.fr>
2022-11-13 14:20:03 +0100

    ex01 done

---
+++ ex01/src/Dog.cpp
@@ -0,0 +1,44 @@
+#include "Dog.hpp"
+
+Dog::Dog(void): Animal()
+{
+	this->type = "Dog";
+	this->brain = new Brain();
+	std::cout << "Dog default constructor called." << std::endl;
+	return ;
+}
+
+Dog::Dog(Dog const &rhs): Animal(rhs)
+{
+	*this = rhs;
+	std::cout << "Dog copy constructor called." << std::endl;
+	return ;
+}
+
+Dog::~Dog(void)
+{
+	delete brain;
+	std::cout << "Dog destructor called." << std::endl;
+	return ;
+}
+
+Dog &Dog::operator=(Dog const &rhs)
+{
+	if (this == &rhs)
+		return (*this);
+	this->type = rhs.type;
+	if (this->brain)
+		delete this->brain;
+	this->brain = new Brain();
+	return (*this);
+}
+
+void Dog::makeSound(void) const
+{
+	std::cout << "Wooof !" << std::endl;
+}
+
+void Dog::setIdea(int const index, std::string const idea)
+{
+	this->brain->ideas[index] = idea;
+}
